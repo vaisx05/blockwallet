2518324ef4b46039e4944a0875e389a465d07d08
Vaishak T <62879169+vaisx05@users.noreply.github.com>
2025-02-25 23:02:19 +0530

    removed ethereum_service.dart

--- lib/services/ethereum_service.dart
+++ lib/services/ethereum_service.dart
@@ -5,19 +5,15 @@ import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart';
 
 class EthereumService {
-  //final String rpcUrl = "http://172.168.11.183:7545"; // Ganache RPC URL
-  final String rpcUrl = "http://172.17.17.210:7545"; // For Android Emulator
-  //final String rpcUrl = "http://127.0.0.1:7545"; // For Android Emulator
-
-  //final String privateKey =
-  //  "0x1a7013d295cea4985e88dc4f5891351fbcb02fb95524a939664726c4e7b86485"; //account 1
+ 
+  final String rpcUrl = "http://ip:port"; // For Android Emulator
+ 
 
   final String privateKey =
-      "0xfdcd05de0be19a735c3669a3815d0ea240efc99b3e93f9b6bf35c8c4b90bfa1e";
+      "";
 
-  final String contractAddress = "0x1dfcde8331779a3ca41e789620ce5afaa5871f86";
-  //final String contractAddress = "0x43b7385A6d048dE2f1727E688A6Cdd9638B48905";
-  //   "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8"; // Contract Address
+  final String contractAddress = "";
+
 
   late Web3Client client;
   late Credentials credentials;
